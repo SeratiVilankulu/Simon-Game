f2255f71ec3f73435c72507df2fc29745219d0ff
SeratiVilankulu <seratimotla@gmail.com.com>
2025-03-05 16:36:47 +0200

    restructuring

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const buttonColours = ["green", "red", "yellow", "blue"];
 let gamePattern = []; // The games pattern
 let userClickedPattern = []; // Patter in which the user clicked the colours
 
+// Check which colour is clicked
+$(".btn").click(function () {
+	let userChosenColour = this.id; // The colour the user has clicked on
+	userClickedPattern.push(userChosenColour);
+	console.log(userClickedPattern);
+
+	$("#" + userChosenColour).addClass("pressed");
+	playSound(userChosenColour);
+
+	// Removing pressed class after some time
+	setTimeout(function () {
+		$("#" + userChosenColour).removeClass("pressed");
+	}, 100);
+});
+
+// Sequence of the game
 function nextSequence() {
 	randomNumber = Math.floor(Math.random() * 4);
 
@@ -13,29 +29,11 @@ function nextSequence() {
 		.fadeOut()
 		.fadeIn();
 
-	let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-	audio.play();
+	playSound(randomChosenColour);
 }
 
-// Check which colour is clicked
-$(".btn").click(function () {
-	let userChosenColour = this.id; // The colour the user has clicked on
-	userClickedPattern.push(userChosenColour);
-	console.log(userClickedPattern);
-
-	// Playing sound and add pressed class
-	function playSound(colour) {
-		let audio = new Audio("sounds/" + colour + ".mp3");
-		return audio.play();
-	}
-
-	$("#" + userChosenColour).addClass("pressed");
-	playSound(userChosenColour);
-
-	// Removing pressed class after some time
-	setTimeout(function () {
-		$("#" + userChosenColour).removeClass("pressed");
-	}, 100);
-});
-
-nextSequence();
+// Playing sound
+function playSound(colour) {
+	let audio = new Audio("sounds/" + colour + ".mp3");
+	return audio.play();
+}
